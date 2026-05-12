eb44e79a6ad24ff65abc38841dc66c69318033f1
sokreach <sautsokreach@gmail.com>
2026-05-12 07:26:24 +0700

    test

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasourceUrl: process.env.DATABASE_URL!,
     log: ["error"],
   });
 
